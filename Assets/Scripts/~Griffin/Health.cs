29bc22f7f541c45ba9dfc69527bddfddfa2ac12a
KingSaje <griffin.pesklevits@gmail.com>
2023-10-08 17:01:46 -0700

    Health Script Created
    Just a little commit to add a basic health script that is easily modifiable and can be added to the player and to enemies.

---
+++ Assets/Scripts/~Griffin/Health.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float maximumHealth = 100f;
+    [SerializeField] private float currentHealth;
+    [SerializeField] private float healthRegen; // flat health regen per second
+
+    private void Start()
+    {
+        currentHealth = maximumHealth;
+    }
+    private void Update()
+    {
+        RegenerateHealth();
+    }
+
+    private void RegenerateHealth()
+    {
+        currentHealth = Mathf.Clamp(currentHealth + (healthRegen * Time.deltaTime), 0, maximumHealth);
+    }
+
+    public void HealForAmount(float heal)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + heal, 0, maximumHealth);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - damage, 0 , maximumHealth);
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+            // Die
+            // Not sure exactly how I want to go about this yet, still brainstorming
+    }
+
+}
