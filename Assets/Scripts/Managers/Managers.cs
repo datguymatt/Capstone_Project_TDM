3bce0543bad1ef04c53f61a9e5ccf35bdc7b261d
datguymatt <128839182+datguymatt@users.noreply.github.com>
2023-11-10 17:11:59 -0800

    Fixed the map issues
    -some things weren't lined correctly because of how I created them in probuilder at first, I fixed that by re-creating with the same dimensions in Blender and then importing it
    -tweaked the current player controller (unpacked) in my scene just to try some stuff)

---
+++ Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    //Global Variables
+
+    //Singleton Instance
+
+    //Round Timing + Spawn
+
+    //Spawning
+
+    //Coordinate UI Manager stuff
+
+    //Event Management?
+
+
+}
