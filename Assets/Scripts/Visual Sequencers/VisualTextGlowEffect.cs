cdd25b77f40232cf51ee665a7b8a22d08ca3036e
datguymatt <128839182+datguymatt@users.noreply.github.com>
2023-12-07 20:42:32 -0800

    added glow  and fade in effect to the press any button to proceed

--- Assets/Scripts/Visual Sequencers/VisualTextGlowEffect.cs
+++ Assets/Scripts/Visual Sequencers/VisualTextGlowEffect.cs
@@ -1,27 +1,36 @@
 using DG.Tweening;
+using System.Numerics;
+using TMPro;
 using UnityEngine;
 
 public class VisualTextGlowEffect : MonoBehaviour
 {
     public Material textShader;
+    public TextMeshProUGUI _textMeshPro;
     public float start;
     public float end = 1;
     public float duration;
     // Start is called before the first frame update
     void Start()
     {
-        start = textShader.GetFloat("_GlowPower"); 
+        textShader.SetFloat("_GlowPower", 0);
+        FadeIn();
         GlowUp();
     }
 
+    public void FadeIn()
+    {
+        DOTween.To(() => _textMeshPro.alpha, x => _textMeshPro.alpha = x, 1, 5).SetEase(Ease.InSine);
+
+    }
     public void GlowUp()
     {
-        textShader.DOFloat(end, "_GlowPower", duration).OnComplete(GlowDown).SetEase(Ease.InElastic);
+        textShader.DOFloat(end, "_GlowPower", duration).OnComplete(GlowDown).SetEase(Ease.InOutSine);
     }
 
     public void GlowDown()
     {
-        textShader.DOFloat(start, "_GlowPower", duration).OnComplete(GlowUp).SetEase(Ease.InElastic);
+        textShader.DOFloat(start, "_GlowPower", duration).OnComplete(GlowUp).SetEase(Ease.InOutSine);
     }
 
     
