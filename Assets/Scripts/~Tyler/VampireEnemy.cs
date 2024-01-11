10d1efddbc40dd638c83e9037ec6c79e9452eac4
Tyler Penner <tylercalumpenner@gmail.com>
2024-01-10 19:42:21 -0800

    Statetracker fix

--- Assets/Scripts/~Tyler/VampireEnemy.cs
+++ Assets/Scripts/~Tyler/VampireEnemy.cs
@@ -3,15 +3,17 @@ using UnityEngine;
 public class VampireEnemy : Enemy
 {
     [SerializeField] private float maxHealth;
+    private EnemyStateManager stateManager;
     private void Start()
     {
         this.health = new Health(maxHealth);
+        stateManager = GetComponent<EnemyStateManager>();
     }
     public override void Die(string name)
     {
         base.Die(name);
-        
-        //
+        stateManager.ChangeState(stateManager.findPlayerState);
+
         Destroy(this.gameObject);
         //implement an enemy die event to tell managers an enemy died and to tell statemanager to exit state
     }
